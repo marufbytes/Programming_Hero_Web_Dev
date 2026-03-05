0f3559a713cc657310911ffeda0e45ffbaef232d
marufbytes <marufahammed215@gmail.com>
2026-03-05 18:41:05 +0600

    JS Basic

---
+++ Module_13/13_4/var.js
@@ -0,0 +1,9 @@
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2005;
+
+var age= 17;
+age=35;
+
+console.log(age);
---
+++ Module_13/13_6/types.js
@@ -0,0 +1,12 @@
+var name = "Maruf";
+var address="Kuratoli,Kuril";
+var country="Bangladesh"
+
+var isRich = true;
+
+var passed = false;
+
+console.log(passed);
+
+console.log(typeof passed);
+console.log(typeof country);
