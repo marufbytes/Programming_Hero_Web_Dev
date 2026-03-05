9b02ef9202139fb9140e29fba87c27c263b7a24d
marufbytes <marufahammed215@gmail.com>
2026-03-05 19:28:07 +0600

    Condition

---
+++ Module_14/Compare.js
@@ -0,0 +1,19 @@
+//Ternary -> Three parts
+
+var age=12;
+age>=18? console.log("Vote please!"):console.log("No vote")
+
+let price =500;
+const isLeader =true;
+
+// if(isLeader===true){
+//     price=0;
+// }
+// else{
+//     price+=10;
+// }
+
+// console.log(price);
+
+price= isLeader===true?0:price+100;
+console.log(price);
