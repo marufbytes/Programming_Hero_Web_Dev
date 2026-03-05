5376478eae05e35455cd9d91c786df8d5153eed6
marufbytes <marufahammed215@gmail.com>
2026-03-05 18:57:59 +0600

    Variable

---
+++ Module_13/13_7/naming.js
@@ -0,0 +1,14 @@
+var orangePrice = 20;
+var chocolate = 0.5
+// var assple = parseInt('aefadf ');
+var applePrice = parseFloat('22.5');
+
+// console.log(orangePrice+assple);
+console.log(applePrice+applePrice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first+second;
+console.log(total.toFixed(2));
+
+var c = -50/0;
