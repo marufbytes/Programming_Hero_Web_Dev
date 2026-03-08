db3de70638b937c717bd9b00dfc9578052f6e2c4
marufbytes <marufahammed215@gmail.com>
2026-03-09 04:30:10 +0600

    isArray

---
+++ Module_14/isArray.js
@@ -0,0 +1,11 @@
+const friends = ["kalam", "Gelam", "Khailam", "Dekhlam"];
+
+const nums =[];
+const food = 'ros o golla';
+
+const age = 15;
+
+console.log(Array.isArray(friends))
+console.log(Array.isArray(nums))
+console.log(Array.isArray(food))
+console.log(Array.isArray(age))
