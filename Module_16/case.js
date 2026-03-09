47832655a2db219d612dd0eeeb4a2ed624a302ab
marufbytes <marufahammed215@gmail.com>
2026-03-09 18:29:05 +0600

    String

---
+++ Module_16/case.js
@@ -0,0 +1,24 @@
+const school = 'Milestone College';
+
+console.log(school.toLocaleLowerCase());
+
+const subject = 'Chemistry';
+const book ='chemistry';
+
+if (subject===book){
+    console.log('I am reading book');
+}
+else{
+    console.log('hudai hudai');
+}
+
+const drink = 'water';
+const liquid = ' water';
+
+if(drink.trim()=== liquid){
+    console.log('pani er opor nam jjibob');
+}
+
+else{
+    console.log("go there");
+}
