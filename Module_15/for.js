4fac61efa3c8431384bb7ef21dec0dbd6f05b4f9
marufbytes <marufahammed215@gmail.com>
2026-03-09 14:11:27 +0600

    For loop in JS

---
+++ Module_15/for.js
@@ -0,0 +1,15 @@
+const number = [2,45,45,4,7,3,5];
+
+console.log(number);
+
+for (const num of number){
+    console.log(num);
+}
+
+const fruis=["aam","Jaam", "Kola", "Lichu"];
+console.log(fruis);
+
+for(const fruit of fruis){
+    console.log(fruit);
+    console.log('I want to eat');
+}
