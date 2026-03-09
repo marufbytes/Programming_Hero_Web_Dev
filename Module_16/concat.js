fa7024582976a462da814e27d7aebfd3663cf10d
marufbytes <marufahammed215@gmail.com>
2026-03-09 19:42:34 +0600

    Concat

---
+++ Module_16/concat.js
@@ -0,0 +1,10 @@
+const fristName = "Abid";
+const lastName = "Ahmed";
+
+const fullName1= fristName+" "+lastName;
+console.log(fullName1);
+
+const fullName2 = fristName.concat(' ').concat(lastName);
+console.log(fullName2);
+
+console.log(lastName.includes('x'));
