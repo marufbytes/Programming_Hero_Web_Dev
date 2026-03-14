f542ddb09952a513b708f0335a94be99bbb631f7
marufbytes <marufahammed215@gmail.com>
2026-03-14 13:19:08 +0600

    Keys

---
+++ Module_16/keys.js
@@ -0,0 +1,13 @@
+const computer={
+    brand:"Lenovo",
+    price:5000,
+    processor:"Inter",
+    hdd: "512gb",
+    monitor: "hp"
+}
+
+const keys = Object.keys(computer);
+console.log(keys);
+
+const values = Object.values(computer);
+console.log(values);
