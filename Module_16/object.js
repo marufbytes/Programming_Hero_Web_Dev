386553686546d404c6f315010f6d1d9596995d12
marufbytes <marufahammed215@gmail.com>
2026-03-14 12:59:16 +0600

    Object

---
+++ Module_16/object.js
@@ -0,0 +1,26 @@
+const age=21;
+const school='Milestone College';
+const isPassed=true;
+// let isDeveloper;
+const subjects=["bangla","English", "Physics","Math"];
+ 
+
+//object
+
+const bottle = {
+    brand: 'Mum',
+    price: 20,
+    color: 'White',
+    isClean: false
+}
+
+const subject={
+    name: "Biology",
+    teacher: "Alamgir Hossain",
+    examDate: "30 Freb",
+    chapeters: ['first','second','third'],
+    exams:{
+        name: 'final',
+        marks: 100
+    }
+}
