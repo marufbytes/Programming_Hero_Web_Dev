f319f5f4b85a784d70253bad079bbeb7837c12a8
marufbytes <marufahammed215@gmail.com>
2026-03-17 00:58:06 +0600

    Function declaration

---
+++ Module_17/function.js
@@ -0,0 +1,15 @@
+const number =45;
+
+function fanOff (){
+    console.log("Data");
+    console.log("Second");
+    console.log("Click");
+}
+
+function brushTeeth(){
+    console.log("Brush");
+    console.log("Do this");
+}
+
+fanOff();
+brushTeeth();
