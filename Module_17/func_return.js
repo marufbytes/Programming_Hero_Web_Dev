678fe5a5f6e99bfa39cb8e965d3e161408dfdead
marufbytes <marufahammed215@gmail.com>
2026-03-17 05:09:33 +0600

    Function

---
+++ Module_17/func_return.js
@@ -0,0 +1,12 @@
+function tenTime(number){
+    const result=number*10;
+    return result;
+}
+
+function cutHalf(number){
+    const half =number/2;
+    return half;
+}
+
+const result = tenTime(5);
+console.log("output:", result);
