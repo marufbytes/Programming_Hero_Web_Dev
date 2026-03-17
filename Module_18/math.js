19aacbc5282ad53b96b052e061f8b33b39e9bb1a
marufbytes <marufahammed215@gmail.com>
2026-03-17 22:15:31 +0600

    Math

---
+++ Module_18/math.js
@@ -0,0 +1,16 @@
+const min = Math.min(45,34,533,54);
+console.log(min);
+
+const max = Math.max(34,53,3,543,5430);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(5-88));  //gap
+console.log(Math.round(3.53));
+console.log(Math.round(3.41));
+console.log(Math.floor(2.99));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+const rand = Math.round(Math.random()*10);
+console.log(rand);
