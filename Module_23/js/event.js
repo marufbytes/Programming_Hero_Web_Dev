0a53ef712f1326bad61da93f707da58061ae627a
marufbytes <marufahammed215@gmail.com>
2026-04-04 01:59:05 +0600

    Onclick

--- Module_23/js/event.js
+++ Module_23/js/event.js
@@ -1,1 +1,16 @@
 console.log("Connected");
+
+function makeYellow() {
+    document.body.style.backgroundColor = 'yellow';
+}
+
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// 3. get element by id and then set on "ONCLICK"
+const btnMakeBlue = document.getElementById('btn-make-blue');
+        console.log(btnMakeBlue);
+        btnMakeBlue.onclick=function makeBlue(){
+            document.body.style.backgroundColor='blue';
+        }
