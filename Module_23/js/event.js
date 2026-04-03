4489582cbff9a23fe6e1040132fd9825260d0bb3
marufbytes <marufahammed215@gmail.com>
2026-04-04 02:16:40 +0600

    Evenlistener

--- Module_23/js/event.js
+++ Module_23/js/event.js
@@ -10,7 +10,8 @@ function makeRed() {
 
 // 3. get element by id and then set on "ONCLICK"
 const btnMakeBlue = document.getElementById('btn-make-blue');
-        console.log(btnMakeBlue);
-        btnMakeBlue.onclick=function makeBlue(){
-            document.body.style.backgroundColor='blue';
-        }
+
+btnMakeBlue.onclick = function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
