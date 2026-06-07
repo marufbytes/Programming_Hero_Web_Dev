3c98bd943dee8e2a4ae36ddd4ea5ac4e6d41a217
marufbytes <marufahammed215@gmail.com>
2026-06-08 01:46:13 +0600

    Differences between var let const

---
+++ Module_26/let_const.js
@@ -0,0 +1,21 @@
+const name = 'Bangladesh';
+// const name= 'Singapore';
+
+let price = 120;
+price=150;
+console.log(price);
+
+var counttryname = 'Bangladesh'  //Do not use, old version
+
+const dress = ['shirt', 'pand', 'shoe']
+dress.push('belt');
+console.log(dress);
+
+const student = {
+    studentname: 'Maruf',
+    age:25
+}
+
+student.name= 'John';
+student.age=30;
+console.log(student);
