bb2453cc02c0a655a0b77c7b79c7e87e0cf6c814
marufbytes <marufahammed215@gmail.com>
2026-06-09 02:20:13 +0600

    Distructuring

---
+++ Module_26/distructuring.js
@@ -0,0 +1,20 @@
+// const product={
+//     name:'iphone',
+//     price: 799,
+//     descriptio:'A smartphone'
+// };
+
+// const newPrice = product.price+100;
+// const phoneName=`this is ${product.name}`;
+
+// const price = product.price;
+// const name = product.name;
+
+
+// const product={name:'iphone',price: 799, brand:'A smartphone'};
+const {name,price,brand,camera='12MP'}={name:'iphone',price: 799, brand:'A smartphone',camera:'48MP'};
+console.log(name);
+console.log(camera);
+
+const [first,second]=[10,20,30,40,50,60];
+console.log(first,second);
