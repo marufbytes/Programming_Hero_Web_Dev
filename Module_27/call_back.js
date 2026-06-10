3e6e25e7b29f49b0ff904ee4739f4064f1894d70
marufbytes <marufahammed215@gmail.com>
2026-06-10 20:54:11 +0600

    Call back function asparameter

---
+++ Module_27/call_back.js
@@ -0,0 +1,17 @@
+document.getElementById('#btn').addEventListener('click',function(event){
+    
+})
+
+addEventListener('click',function(event){})
+
+function settleLife(name, isBCS, marriage, patri){
+    if(isBCS){
+        marriage(patri)
+    }
+}
+function boloKobul(patri){
+    console.log("Kobul");
+}
+
+settleLife("Tomal",true,boloKobul, 'nari');
+boloKobul('Pori');
